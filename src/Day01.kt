44187f3907a867ca44695acf09ba0d17a60b5ea4
RandomUserIK <kovac.ivan97@icloud.com>
2022-12-01 19:37:40 +0100

    - implement first challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	fun getElfCalories(input: List<String>): List<Int> {
+		var calories = 0
+		val elfCalories = mutableListOf<Int>() // priorityQueue better
+		input.forEach { cal ->
+			when {
+				cal.isNotBlank() -> calories += cal.toInt()
+				else -> {
+					elfCalories.add(calories)
+					calories = 0
+				}
+			}
+		}
+		return elfCalories.toList()
+	}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+	fun part1(input: List<String>): Int = getElfCalories(input).max()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+	fun part2(input: List<String>): Int = getElfCalories(input).sortedDescending().take(3).sum()
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+	val input = readInput("day01_input")
+	println(part1(input))
+	println(part2(input))
 }
