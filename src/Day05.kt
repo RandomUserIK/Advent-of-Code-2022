b34701589fc8f1ac48adfbdec3cbe22acfa063f2
RandomUserIK <kovac.ivan97@icloud.com>
2022-12-05 23:51:56 +0100

    - implement day 5 part 1
    Signed-off-by: RandomUserIK <kovac.ivan97@icloud.com>

---
+++ src/Day05.kt
@@ -0,0 +1,54 @@
+import java.util.*
+
+private const val STACK_DISTANCE = 4
+
+data class Directive(
+	val numberOfCrates: Int,
+	val from: Int,
+	val to: Int,
+)
+
+fun List<String>.toStacks(): List<Stack<Char>> {
+	val numberOfStacks = last().split("  ").size
+	val result = List(numberOfStacks) { Stack<Char>() }
+	takeWhile { it.contains("[") }.reversed().forEach { crates ->
+		crates.forEachIndexed { idx, symbol ->
+			if (symbol.isUpperCase())
+				result[idx / STACK_DISTANCE].push(symbol)
+		}
+	}
+	return result
+}
+
+fun List<String>.toDirectives(): List<Directive> =
+	map {
+		Directive(
+			numberOfCrates = it.substringAfter("move ").substringBefore(" from").toInt(),
+			from = it.substringAfter("from ").substringBefore(" to").toInt() - 1,
+			to = it.substringAfter("to ").toInt() - 1
+		)
+	}
+
+fun main() {
+	fun getStacksAndDirectives(input: List<String>): Pair<List<Stack<Char>>, List<Directive>> =
+		Pair(
+			input.takeWhile { it.isNotEmpty() }.toStacks(),
+			input.takeLastWhile { it.isNotEmpty() }.toDirectives()
+		)
+
+	fun part1(crateStacks: List<Stack<Char>>, directives: List<Directive>) {
+		directives.forEach { directive ->
+			repeat(directive.numberOfCrates) { crateStacks[directive.to].push(crateStacks[directive.from].pop()) }
+		}
+		println(crateStacks.map { it.last() }.joinToString(""))
+	}
+
+	fun part2() {
+
+	}
+
+	val input = readInput("inputs/day05_input")
+	val (crateStacks, directives) = getStacksAndDirectives(input)
+	part1(crateStacks, directives)
+	part2()
+}
