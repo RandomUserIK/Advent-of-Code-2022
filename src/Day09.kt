976464cc4236b66147164a8e0ae7d60e908b407f
RandomUserIK <kovac.ivan97@icloud.com>
2022-12-11 20:33:02 +0100

    - implement day 9 part 1
    Signed-off-by: RandomUserIK <kovac.ivan97@icloud.com>

---
+++ src/Day09.kt
@@ -0,0 +1,83 @@
+import kotlin.math.abs
+
+private fun List<String>.toDirections(): List<Direction> =
+	joinToString("") {
+		val direction = it.substringBefore(" ")
+		val steps = it.substringAfter(" ").toInt()
+		direction.repeat(steps)
+	}.map {
+		when (it) {
+			'U' -> Direction.U
+			'D' -> Direction.D
+			'L' -> Direction.L
+			'R' -> Direction.R
+			else -> error("Invalid direction entry provided: [$it].")
+		}
+	}
+
+
+private infix fun Point.touches(tail: Point): Boolean =
+	abs(this.row - tail.row) <= 1 && abs(this.col - tail.col) <= 1
+
+
+private infix fun Point.chase(head: Point): Point =
+	(head - this).let { diff ->
+		this + Point(row = diff.row.coerceIn(-1, 1), col = diff.col.coerceIn(-1, 1))
+	}
+
+
+private enum class Direction(
+	val dRow: Int,
+	val dCol: Int,
+) {
+	U(-1, 0),
+	D(1, 0),
+	L(0, -1),
+	R(0, 1)
+}
+
+private data class Point(
+	val row: Int = 0,
+	val col: Int = 0,
+) {
+	fun move(direction: Direction): Point = copy(row = row + direction.dRow, col = col + direction.dCol)
+
+	operator fun plus(other: Point) =
+		Point(
+			row = row + other.row,
+			col = col + other.col,
+		)
+
+	operator fun minus(other: Point) =
+		Point(
+			row = row - other.row,
+			col = col - other.col,
+		)
+}
+
+
+fun main() {
+	fun part1(input: List<String>): Int {
+		val directions = input.toDirections()
+		var head = Point()
+		var tail = Point()
+		var pointsVisited = mutableSetOf(Point()) // we ought to include the starting point
+
+		directions.forEach { direction ->
+			head = head.move(direction)
+			if (!(head touches tail)) {
+				tail = tail chase head
+			}
+			pointsVisited.add(tail)
+		}
+		return pointsVisited.size
+	}
+
+	fun part2(input: List<String>) {
+
+	}
+
+	val input = readInput("inputs/day09_input")
+	println(part1(input))
+	println(part2(input))
+}
