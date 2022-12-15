3b415ed9fc20dbc1919de28d600fce01a0aa67b0
RandomUserIK <kovac.ivan97@icloud.com>
2022-12-15 21:57:48 +0100

    - implement day 11 part 1
    Signed-off-by: RandomUserIK <kovac.ivan97@icloud.com>

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,32 +1,43 @@
 import java.io.File
 
-private data class Item(
-	val worryLevel: Int,
-)
+private const val WORRY_LEVEL_DIVISOR = 3
+private const val NUMBER_OF_ROUNDS = 20
 
 private data class Monkey(
 	val index: Int,
-	val items: MutableList<Item> = mutableListOf(),
-	val operation: (Int) -> Int,
-	val test: (Int) -> Int,
+	val items: MutableList<Int> = mutableListOf(),
+	val inspectOperation: (Int) -> Int,
+	val test: Int,
+	val trueMonkey: Int,
+	val falseMonkey: Int,
 	var activity: Int = 0,
-)
+) {
+	fun takeTurn(monkeys: List<Monkey>) {
+		items.forEach { item ->
+			val (throwTo, newItem) = inspectAndPlay(item)
+			monkeys[throwTo].items.add(newItem)
+		}
+		activity += items.size
+		items.clear()
+	}
+
+	override fun toString(): String =
+		"Monkey $index: ${items.map { it }.joinToString(", ")}"
+
+	private fun inspectAndPlay(item: Int): Pair<Int, Int> {
+		val newItem = inspectOperation(item) / WORRY_LEVEL_DIVISOR
+		val throwTo = if (newItem % test == 0) trueMonkey else falseMonkey
+		return Pair(throwTo, newItem)
+	}
+}
 
 private fun String.toOperation(): (Int) -> Int =
 	split(" ").let {
 		when {
-			it.first() == "*" -> fun(input: Int) = it.last().toInt() * input
-			else -> fun(input: Int) = it.last().toInt() + input
+			it.last() == "old" -> ({ input -> input * input })
+			it.first() == "*" -> ({ input -> it.last().toInt() * input })
+			else -> ({ input -> it.last().toInt() + input })
 		}
-	}
-
-private fun String.toTestFun(): (Int) -> Int =
-	split(" ").let {
-		fun(input: Int) =
-			when (it.first().toInt() % input == 0) {
-				true -> it[1].toInt()
-				else -> it.last().toInt()
-			}
 	}
 
 private fun String.toMonkey() =
@@ -38,26 +49,24 @@ private fun String.toMonkey() =
 		items = substringAfter("Starting items: ")
 			.substringBefore("\n")
 			.split(", ")
-			.map { Item(it.toInt()) }
+			.map { it.toInt() }
 			.toMutableList(),
 
-		operation = substringAfter("new = old ")
+		inspectOperation = substringAfter("new = old ")
 			.substringBefore("\n")
 			.toOperation(),
 
-		test = (
-				substringAfter("divisible by ")
-					.substringBefore("\n") + " " + substringAfter("If true: throw to monkey ")
-					.substringBefore("\n") + " " + substringAfter("If false: throw to monkey ")
-					.substringBefore("\n")
-				).toTestFun()
+		test = substringAfter("divisible by ").substringBefore("\n").toInt(),
+		trueMonkey = substringAfter("If true: throw to monkey ").substringBefore("\n").toInt(),
+		falseMonkey = substringAfter("If false: throw to monkey ").substringBefore("\n").toInt()
 	)
 
 fun main() {
 	fun part1(input: List<String>) {
 		val monkeys = input.map { it.toMonkey() }
-
-		// TODO
+		repeat(NUMBER_OF_ROUNDS) { monkeys.forEach { it.takeTurn(monkeys) } }
+		val sorted = monkeys.sortedByDescending { it.activity }
+		println("Level of monkey business: ${sorted.first().activity * sorted[1].activity}")
 	}
 
 	fun part2(input: List<String>) {
@@ -65,6 +74,5 @@ fun main() {
 	}
 
 	val input = File("src/inputs/day11_input.txt").readText().split("\n\n")
-	println(part1(input))
-	println(part2(input))
+	part1(input)
 }
